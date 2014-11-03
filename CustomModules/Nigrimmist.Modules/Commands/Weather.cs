2825f95ed20f31aa7afdd1cb9c77be69c0eebd53
Nigrimmist <Nigrimmist@gmail.com>
2014-11-03 23:31:16 +0200

    - weather

--- CustomModules/Nigrimmist.Modules/Commands/Weather.cs
+++ CustomModules/Nigrimmist.Modules/Commands/Weather.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-
+using System.Text.RegularExpressions;
+using System.Web;
 using HelloBotCommunication;
 using HelloBotModuleHelper;
 using HtmlAgilityPack;
@@ -14,25 +16,53 @@ namespace Nigrimmist.Modules.Commands
         public List<string> CallCommandList {
             get { return new List<string>() { "погода" }; }
         }
-        public string CommandDescription { get { return @"Погода с тутбая для Минска. ""!погода"" = текущая+завтра"; } }
+        public string CommandDescription { get { return @"Погода на сегодня и завтра. !погода {город}"; } }
         public void HandleMessage(string args, object clientData, Action<string> sendMessageFunc)
         {
+            if (string.IsNullOrEmpty(args))
+                args = "минск";
+
             HtmlReaderManager hrm = new HtmlReaderManager();
-            hrm.Get("http://pogoda.tut.by/");
+            hrm.Get(string.Format("http://pogoda.yandex.by/{0}", HttpUtility.UrlEncode(args)));
             string html = hrm.Html;
             HtmlDocument htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(html);
-            var tds = htmlDoc.DocumentNode.SelectNodes(@"//./td[@class='fcurrent-top' or @class='fcurrent-s']");
+            var todayContainers = htmlDoc.DocumentNode.SelectNodes(@"//./div[@class='b-thermometer']");
             StringBuilder sb = new StringBuilder();
-            sb.Append("Погода в Минске :");
-            foreach (var td in tds)
+
+            string currTemp = todayContainers[0].SelectSingleNode(".//.//div[@class='b-thermometer__now']").InnerText;
+            string currTempDescr = todayContainers[0].SelectSingleNode(".//.//div[@class='b-info-item b-info-item_type_fact-big']").InnerText;
+            string windSpeed = htmlDoc.DocumentNode.SelectNodes(".//.//div[@class='b-thermometer-info__line']")[1].InnerText;
+            windSpeed = ClearText(windSpeed);
+
+            sb.AppendLine(string.Format("За окном : {0} {1} {2}", currTemp, currTempDescr, windSpeed));
+
+            for (int i = 1; i < todayContainers.Count; i++)
             {
-                sb.Append(td.SelectSingleNode(".//./div[@class='fcurrent-h']").InnerText + " ");
-                sb.Append(td.SelectSingleNode(".//./span[@class='temp-i']").InnerText + " ");
-                sb.Append(td.SelectSingleNode(".//./div[@class='fcurrent-descr']").InnerText + " ");
-                sb.Append(Environment.NewLine);
+                var container = todayContainers[i];
+
+                string timeName = container.SelectSingleNode(".//./div[@class='b-thermometer__name']").InnerText;
+                string temp = container.SelectSingleNode(".//./div[@class='b-thermometer__small-temp']").InnerText;
+                sb.AppendLine(string.Format("{0} {1}", ClearText(timeName), ClearText(temp)));
+            }
+
+            var dateTds = htmlDoc.DocumentNode.SelectNodes("//./div[@class='b-forecast-detailed__data']/tbody/tr");
+            for (int i = 0; i < dateTds.Count; i+=5)
+            {
+                var morningTd = dateTds[i];
+                var dayTd = dateTds[i + 1];
+                var eveningTd = dateTds[i + 2];
+                var nightTd = dateTds[i + 3];
+
             }
             sendMessageFunc(sb.ToString().Replace("&deg;", "°"));
+        }
+
+        private string ClearText(string str)
+        {
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex(@"[ ]{2,}", options);
+            return regex.Replace(str, @" ").Replace("\n", "").Trim();
         }
     }
 }
